25090bfba875567117951d409644b5749cbbb0aa
Anoop Santhanam <anoop.santhanam@gmail.com>
2022-05-04 19:27:22 +0530

    added local list of primary skills

---
+++ src/components/skills/skill-list.js
@@ -0,0 +1,158 @@
+export const primarySkills = [
+  {
+    label: "[Javascript] React",
+    name: "[Javascript] React",
+  },
+  {
+    label: "[Javascript] Angular",
+    name: "[Javascript] Angular",
+  },
+  {
+    label: "[Javascript] Vue",
+    name: "[Javascript] Vue",
+  },
+  {
+    label: "[Javascript] Node.js",
+    name: "[Javascript] Node.js",
+  },
+  {
+    label: "[Cross-Platform] React Native",
+    name: "[Cross-Platform] React Native",
+  },
+  {
+    label: "Python",
+    name: "Python",
+  },
+  {
+    label: "Ruby",
+    name: "Ruby",
+  },
+  {
+    label: "Golang",
+    name: "Golang",
+  },
+  {
+    label: "DevOps",
+    name: "DevOps",
+  },
+  {
+    labeL: "DevSecOps",
+    name: "DevSecOps",
+  },
+  {
+    label: "Cloud Engineering",
+    name: "Cloud Engineering",
+  },
+  {
+    label: "SRE",
+    name: "SRE",
+  },
+  {
+    label: "Java",
+    name: "Java",
+  },
+  {
+    label: "C/Embedded",
+    name: "C/Embedded",
+  },
+  {
+    label: "QA",
+    name: "QA",
+  },
+  {
+    label: "Elixir",
+    name: "Elixir",
+  },
+  {
+    label: "Scala",
+    name: "Scala",
+  },
+  {
+    label: "PHP",
+    name: "PHP",
+  },
+  {
+    label: ".NET",
+    name: ".NET",
+  },
+  {
+    label: "iOS",
+    name: "iOs",
+  },
+  {
+    label: "Android",
+    name: "Android",
+  },
+  {
+    label: "Flutter",
+    name: "Flutter",
+  },
+  {
+    label: "Unity",
+    name: "Unity",
+  },
+  {
+    label: "Data Engineering",
+    name: "Data Engineering",
+  },
+  {
+    label: "Data Analyst",
+    name: "Data Analyst",
+  },
+  {
+    label: "Data Science",
+    name: "Data Science",
+  },
+  {
+    label: "Salesforce - Admin",
+    name: "Salesforce - Admin",
+  },
+  {
+    label: "Salesforce - Developer",
+    name: "Salesforce - Developer",
+  },
+  {
+    label: "Mulesoft",
+    name: "Mulesoft",
+  },
+  {
+    label: "UX Writer",
+    name: "UX Writer",
+  },
+  {
+    label: "UX Researcher",
+    name: "UX Researcher",
+  },
+  {
+    label: "UI/UX Designer",
+    name: "UI/UX Designer",
+  },
+  {
+    label: "UI(Visual) Designer",
+    name: "UI(Visual) Designer",
+  },
+  {
+    label: "Interaction Designer",
+    name: "Interaction Designer",
+  },
+  {
+    label: "Digital Project Manager",
+    name: "Digital Project Manager",
+  },
+  {
+    label: "Technical Product Manager",
+    name: "Technical Product Manager",
+  },
+  {
+    label: "Product Manager",
+    name: "Product Manager",
+  },
+  {
+    label: "Product Owner",
+    name: "Product Owner",
+  },
+  {
+    label: "Scrum Master",
+    name: "Scrum Master",
+  },
+]
